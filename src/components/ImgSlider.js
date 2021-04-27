56580a3de30089d1d7d5d7504bb3ad8b80f91685
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-04-27 19:30:18 +0200

    add images and style Carousel

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -15,20 +15,56 @@ const ImgSlider = (props) => {
     }
 
     return (
-        <div>
-            <Slider {...settings}>
-                <div>
-                    <h3>1</h3>
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-            </Slider>
-        </div>
+        <Carousel {...settings}>
+            <div>
+                <h3>1</h3>
+            </div>
+            <div>
+                <h3>2</h3>
+            </div>
+            <div>
+                <h3>3</h3>
+            </div>
+        </Carousel>
     )
 }
+
+const Carousel = styled(Slider)`
+    margin-top: 20px;
+    & > button{
+        opacity: 0;
+        height: 100%;
+        width: 5vw;
+        z-index: 1;
+
+        &:hover{
+            opacity: 1;
+            transition: opacity 0.2s ease 0s;
+        }
+    }
+
+    ul li button{
+        &:before{
+            font-size: 10px;
+            color: rgb(150, 158, 171);
+        }
+    }
+
+    li.slick-active button:before{
+        color: white;
+    }
+
+    .slick-list {
+        overflow: initial;
+    }
+
+    .slick-prev{
+        left: -75px;
+    }
+
+    .slick-next{
+        right: -75px;
+    }
+`
 
 export default ImgSlider
