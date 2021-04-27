28623bfa253d3b93da5c710a039aa1662649f414
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-04-27 11:55:43 +0200

    logo to the navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components'
 
 const Header = (props) => {
-    return <Nav>Header</Nav>
+    return <Nav>
+        <Logo>
+            <img src="images/logo.svg" alt="Disney +" />
+        </Logo>
+    </Nav>
 }
 
 const Nav = styled.nav`
@@ -18,6 +22,19 @@ const Nav = styled.nav`
     padding: 0px 36px;
     letter-spacing: 16px;
     z-index: 3;
+`
+
+const Logo = styled.a`
+padding: 0px;
+width: 80px;
+margin-top: 4px;
+max-height: 70px;
+font-size: 0;
+display: inline-block;
+img{
+    display: block;
+    width: 100%;
+}
 `
 
 export default Header
