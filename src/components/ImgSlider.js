5695722d19df8117ee350dac19e899416c66b744
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-04-27 19:15:06 +0200

    ImgSlider components + add react slick ans slick-carousel

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const ImgSlider = (props) => {
+    return (
+        <div>
+            <h2>ImgSlider</h2>
+        </div>
+    )
+}
+
+export default ImgSlider
