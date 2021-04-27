563510e697e0fa69404bbb4575ac3b6983908366
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-04-27 19:42:53 +0200

    Make Viewers Component

---
+++ src/components/Viewers.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Viewers = (props) => {
+    return (
+        <div>
+            <h2>Viewers</h2>
+        </div>
+    )
+}
+
+export default Viewers
