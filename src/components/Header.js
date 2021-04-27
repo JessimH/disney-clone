f26cdfe3890b93f8e6dabbf9db897acd7977a2e4
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-04-27 11:50:10 +0200

    add logo svg to project

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,19 @@ const Header = (props) => {
 }
 
 const Nav = styled.nav`
-
+    position: fixed;
+    top:0;
+    left: 0;
+    right: 0;
+    left: 0;
+    height: 70px;
+    background-color: #090b13;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px 36px;
+    letter-spacing: 16px;
+    z-index: 3;
 `
 
 export default Header
