f3ff5cc97840928c5984390386b4906dae2841ce
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-04-28 14:03:47 +0200

    get data from firebase firestore, useEffect and Redux

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,19 +5,66 @@ import Recommends from './Recommends'
 import NewDisney from './NewDisney'
 import Originals from './Originals'
 import Trending from './Trending'
-
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import db from '../firebase'
+import { setMovies } from '../features/movie/movieSlice'
+import { selectUserName } from '../features/user/userSlice'
 
 const Home = (props) => {
-    return (
-        <Container>
-            <ImgSlider />
-            <Viewers />
-            <Recommends />
-            <NewDisney />
-            <Originals />
-            <Trending />
-        </Container>
-    )
+
+    const dispatch = useDispatch()
+    const userName = useSelector(selectUserName)
+    let recommends = []
+    let newDisney = []
+    let originals = []
+    let trending = []
+
+    useEffect(() => {
+        db.collection('movies').onSnapshot((snapchot) => {
+            snapchot.docs.map((doc) => {
+                switch (doc.data().type) {
+                    case 'recommend':
+                        recommends.push({
+                            id: doc.id,
+                            ...doc.data()
+                        })
+                        break
+                    case 'new':
+                        newDisney.push({
+                            id: doc.id,
+                            ...doc.data()
+                        })
+                        break
+                    case 'original':
+                        originals.push({
+                            id: doc.id,
+                            ...doc.data()
+                        })
+                        break
+                    case 'trending':
+                        trending.push({
+                            id: doc.id,
+                            ...doc.data()
+                        })
+                        break
+                }
+            })
+        })
+    })
+
+}
+
+return (
+    <Container>
+        <ImgSlider />
+        <Viewers />
+        <Recommends />
+        <NewDisney />
+        <Originals />
+        <Trending />
+    </Container>
+)
 }
 
 const Container = styled.main`
